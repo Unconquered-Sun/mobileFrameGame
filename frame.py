eac10c7c1efa042a3937bea27431c11085d9c6e9
selpathor <selpathor@gmail.com>
2019-03-14 20:43:18 -0400

    Started adding basic Frame body

--- frame.py
+++ frame.py
@@ -2,5 +2,37 @@ import pymunk
 
 class Frame:
 
-	def __init__(self):
+	def __init__(self, space):
+		self.space = space
+
+	def addFrame(self):
+
+		#body
+		self.bodyBody = pymunk.Body(1, 1666)
+		self.bodyBody.position = (512,500)
+		self.bodyPoly = pymunk.Poly.create_box(self.bodyBody, size=(50,100))
+
+		#head
+		self.headBody = pymunk.Body(1, 1666)
+		self.headBody.position = (517,530)
+		self.headPoly = pymunk.Poly.create_box(self.headBody, size=(30,30))
 		
+		#Legs
+		self.upperLegsBody = pymunk.Body(1, 1666)
+		self.upperLegsBody.position = (517,450)
+		self.upperLegsPoly = pymunk.Poly.create_box(self.upperLegsBody, size=(30,50))
+
+		self.loweLegsBody = pymunk.Body(1, 1666)
+		self.loweLegsBody.position = (517,400)
+		self.lowerLegsPoly = pymunk.Poly.create_box(self.loweLegsBody, size=(30,50))
+
+		#arms
+
+
+
+
+
+		self.space.add(self.bodyBody, self.bodyPoly)
+		self.space.add(self.headBody, self.headPoly)
+		self.space.add(self.upperLegsBody, self.upperLegsPoly)
+		self.space.add(self.loweLegsBody, self.lowerLegsPoly)
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import sys
 import pygame
 import pymunk
 from pymunk import Vec2d
+import pymunk.pygame_util
+from frame import Frame
 
 
 class Game:
@@ -17,6 +19,12 @@ class Game:
 		self.space = pymunk.Space()
 		self.space.gravity = Vec2d(0.0, 0.0)
 
+		testFrame = Frame(self.space)
+		testFrame.addFrame()
+
+		self.options = pymunk.pygame_util.DrawOptions(self.screen)
+		self.space.debug_draw(self.options)
+
 		while True:
 			for event in pygame.event.get():
 				print(event.type)
@@ -25,7 +33,9 @@ class Game:
 				elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
 					sys.exit(0)
 
-			self.screen.fill((255,255,255))
+			self.screen.fill((0,0,0))
+
+			self.space.debug_draw(self.options)
 
 			self.space.step(1/50.0) #3
 
